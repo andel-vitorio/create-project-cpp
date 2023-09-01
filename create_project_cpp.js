f0e15fcad5ccf2084319246b1dcd7c7dcb436d8b
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-08-31 20:02:54 -0400

    feat: add the creation of directories
    Adding the creation of the main directories of a C++ project.

---
+++ create_project_cpp.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
+
+program
+  .version('1.0.0')
+  .arguments('<projectName>')
+  .description('Create a new project in C++')
+  .action((projectName) => {
+    const projectDir = path.join(process.cwd(), projectName);
+    
+    if ( fs.existsSync(projectDir) ) {
+      console.error(`Error: The project '${projectName}' already exists.`);
+      process.exit(1);
+    }
+
+    fs.mkdirSync(projectDir);
+    fs.mkdirSync(path.join(projectDir, 'inc'));
+    fs.mkdirSync(path.join(projectDir, 'lib'));
+    fs.mkdirSync(path.join(projectDir, 'obj'));
+    fs.mkdirSync(path.join(projectDir, 'src'));
+
+    console.log(`Created project '${projectName}' in C++.`);
+  });
+
+program.parse(process.argv);
